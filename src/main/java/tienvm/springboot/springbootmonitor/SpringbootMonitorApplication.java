0b710f44c565dfa17a8e80748fd81c212aa42a48
MinhhTien <92145479+MinhhTien@users.noreply.github.com>
2023-04-29 13:41:37 +0700

    Config Prometheus Monitoring

---
+++ src/main/java/tienvm/springboot/springbootmonitor/SpringbootMonitorApplication.java
@@ -0,0 +1,13 @@
+package tienvm.springboot.springbootmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMonitorApplication.class, args);
+	}
+
+}
